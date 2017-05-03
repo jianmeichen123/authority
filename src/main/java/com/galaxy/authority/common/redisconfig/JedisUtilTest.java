f8baddb0ea80ed78f53f01273c64e6abaa01c6d6
liyanqiao <qyz91888@163.com>
2017-05-03 17:50:17 +0800

    22

--- src/main/java/com/galaxy/authority/common/redisconfig/JedisUtilTest.java
+++ src/main/java/com/galaxy/authority/common/redisconfig/JedisUtilTest.java
@@ -1,67 +1,67 @@
 package com.galaxy.authority.common.redisconfig;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import redis.clients.jedis.Jedis;
-//import redis.clients.jedis.JedisPool;
-//import redis.clients.jedis.JedisPoolConfig;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 
 public class JedisUtilTest {
-//	
-//	/*@Autowired
-//    private JedisPool jedisPool;   
-//      
-//    public void set(String key, String value) throws Exception {  
-//        Jedis jedis = null;  
-//        try {  
-//            jedis = jedisPool.getResource();  
-//            jedis.set(key, value);  
-//        } finally {  
-//            //返还到连接池  
-//            jedis.close();  
-//        }  
-//    }  
-//      
-//    public String get(String key) throws Exception  {  
-//  
-//        Jedis jedis = null;  
-//        try {  
-//            jedis = jedisPool.getResource();  
-//            return jedis.get(key);  
-//        } finally {  
-//            //返还到连接池  
-//            jedis.close();  
-//        }  
-//    }  
-//    
-//    public static void main(String[] args) {
-//    	JedisUtilTest test = new JedisUtilTest();
-//    	try {
-//			test.set("age", "20");
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}*/
-//	    
-//    
-//	//测试redis是否连接上
-//    JedisPool pool;
-//    Jedis jedis;
-//    @Before
-//    public void setUp() {
-//        pool = new JedisPool(new JedisPoolConfig(), "127.0.0.1");
-//        jedis = pool.getResource();
-//    }
-//    @Test
-//    public void testSet(){
-//    	System.out.println(jedis.set("name", "lili"));
-//    }
-//    @Test
-//    public void testGet(){
-//        System.out.println(jedis.get("name"));
-//    }
+	
+	/*@Autowired
+    private JedisPool jedisPool;   
+      
+    public void set(String key, String value) throws Exception {  
+        Jedis jedis = null;  
+        try {  
+            jedis = jedisPool.getResource();  
+            jedis.set(key, value);  
+        } finally {  
+            //返还到连接池  
+            jedis.close();  
+        }  
+    }  
+      
+    public String get(String key) throws Exception  {  
+  
+        Jedis jedis = null;  
+        try {  
+            jedis = jedisPool.getResource();  
+            return jedis.get(key);  
+        } finally {  
+            //返还到连接池  
+            jedis.close();  
+        }  
+    }  
+    
+    public static void main(String[] args) {
+    	JedisUtilTest test = new JedisUtilTest();
+    	try {
+			test.set("age", "20");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}*/
+	    
+    
+	//测试redis是否连接上
+    JedisPool pool;
+    Jedis jedis;
+    @Before
+    public void setUp() {
+        pool = new JedisPool(new JedisPoolConfig(), "127.0.0.1");
+        jedis = pool.getResource();
+    }
+    @Test
+    public void testSet(){
+    	System.out.println(jedis.set("name", "lili"));
+    }
+    @Test
+    public void testGet(){
+        System.out.println(jedis.get("name"));
+    }
     
 }
