690d230f6a5d16aab84a3e004fd0ff7439eda807
Wang Song <songwang@galaxyintrnet.com>
2017-05-09 11:41:54 +0800

    scope

--- src/main/java/com/galaxy/authority/business/login/service/LoginServiceImpl.java
+++ src/main/java/com/galaxy/authority/business/login/service/LoginServiceImpl.java
@@ -7,8 +7,8 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.galaxy.authority.business.user.service.IUserService;
 import com.galaxy.authority.dao.login.ILoginDao;
-import com.galaxy.authority.dao.user.IUserDao;
 
 @Repository
 public class LoginServiceImpl implements ILoginService{
@@ -16,7 +16,7 @@ public class LoginServiceImpl implements ILoginService{
 	@Autowired
 	private ILoginDao dao;
 	@Autowired
-	private IUserDao userDao;
+	private IUserService userService;
 	
 	/**
 	 * 用户登录
@@ -29,7 +29,27 @@ public class LoginServiceImpl implements ILoginService{
 			Map<String, Object> query = new HashMap<>();
 			query.put("userId", userInfo.get("id"));
 			query.put("companyId", userInfo.get("companyId"));
-			List<Map<String, Object>> res = userDao.getUserResources(query);
+			List<Map<String, Object>> res = userService.getUserResources(query);
+			if(res != null)
+			{
+				List<Map<String, Object>> scopeList = userService.getUserScope(query);
+				Map<String,Object> userIdsMap = new HashMap<>();
+				for(Map<String, Object> item : scopeList)
+				{
+					if(item.containsKey("userIds"))
+					{
+						userIdsMap.put(item.get("resourceMark")+"", item.get("userIds"));
+					}
+				}
+				for(Map<String, Object> item : res)
+				{
+					String key = item.get("resourceMark")+"";
+					if(userIdsMap.containsKey(key))
+					{
+						item.put("userIds", userIdsMap.get(key));
+					}
+				}
+			}
 			userInfo.put("allResourceToUser", res);
 		}
 		return userInfo;
