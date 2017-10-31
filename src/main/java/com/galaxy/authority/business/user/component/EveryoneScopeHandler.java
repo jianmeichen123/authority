6509d2a269b7bd3050134f24f072fc22dd97baa9
Wang Song <songwang@galaxyintrnet.com>
2017-10-31 11:29:53 +0800

    数据权限-所有人

--- src/main/java/com/galaxy/authority/business/user/component/EveryoneScopeHandler.java
+++ src/main/java/com/galaxy/authority/business/user/component/EveryoneScopeHandler.java
@@ -1,9 +1,11 @@
 package com.galaxy.authority.business.user.component;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.galaxy.authority.dao.user.IUserDao;
 /**
  * 所有人
  * @author wangsong
@@ -11,7 +13,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class EveryoneScopeHandler extends AbstractScopeHandler implements ScopeHandler {
-
+	@Autowired
+	IUserDao userDao;
 	@Override
 	public boolean support(Object data) {
 		Integer spId = getVal(data,"spId");
@@ -20,7 +23,7 @@ public class EveryoneScopeHandler extends AbstractScopeHandler implements ScopeH
 
 	@Override
 	public List<Integer> handle(Object data) {
-		return new ArrayList<Integer>();
+		return userDao.getUserIdList();
 	}
 
 }
