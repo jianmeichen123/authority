44eedb0a7b9773f0efdf193888f6059b764e2744
xiaochuang <chuangxiao@galaxyinternet.com>
2017-12-18 15:17:54 +0800

    1

--- src/main/java/com/galaxy/authority/business/brain/controller/BrainController.java
+++ src/main/java/com/galaxy/authority/business/brain/controller/BrainController.java
@@ -3,6 +3,7 @@ package com.galaxy.authority.business.brain.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.galaxy.authority.business.brain.service.IBrainService;
+import com.galaxy.authority.common.CUtils;
 
 @Controller
 @RequestMapping("/brain")
@@ -27,12 +29,15 @@ public class BrainController {
 	 */
 	@RequestMapping("selectDepIdByUserId")
 	@ResponseBody
-	public Object selectDepIdByUserId(@RequestBody String userId)
+	public Object selectDepIdByUserId(@RequestBody String paramString)
 	{
 		Long id = 0L;
 		Map<String,Object> paramMap = new HashMap<>();
+		JSONObject paramJson = CUtils.get().object2JSONObject(paramString);
 		try {
-			paramMap.put("userId", userId);
+			if(paramJson!=null && paramJson.has("userId")){
+				paramMap.put("userId", paramJson.getLong("userId"));
+			}
 			id = service.selectDepIdByUserId(paramMap);
 		} catch (Exception e) {
 			log.error(BrainController.class.getName() + "selectDepIdByUserId",e);
